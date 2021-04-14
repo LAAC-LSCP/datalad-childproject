4129def1cbb595732c754d4ff5a083080e736554
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-04-14 10:37:34 +0200

    retrieve annotations metadata

--- datalad_childproject/extractor.py
+++ datalad_childproject/extractor.py
@@ -13,6 +13,9 @@ from datalad.metadata.definitions import vocabulary_id
 from datalad.utils import assure_unicode
 from datalad.support.external_versions import external_versions
 
+from ChildProject.projects import ChildProject
+from ChildProject.annotations import AnnotationManager
+
 from datalad import cfg
 
 import logging
@@ -41,14 +44,14 @@ sex_label_map = {
 
 
 class MetadataExtractor(BaseMetadataExtractor):
-    def get_metadata(self, dataset, content):
+    def _load(self):
+        self.project = ChildProject(self.ds.path)
+        self.am = AnnotationManager(self.project)
+        self.am.read()
 
-        try:
-            recordings = pd.read_csv(os.path.join(self.ds.path, 'metadata/recordings.csv'))
-            children = pd.read_csv(os.path.join(self.ds.path, 'metadata/children.csv'))
-        except Exception as exc:
-            lgr.error("could not read the metadata due to some exception.\n{}".format(str(exc)))
-            return {}, []
+    def _get_dsmeta(self, dataset, content):
+        recordings = self.project.recordings
+        children = self.project.children
 
         ## Extract experiment(s)
         experiment = None
@@ -64,19 +67,19 @@ class MetadataExtractor(BaseMetadataExtractor):
         }
 
         ## Extract sample size
-        dsmeta['children'] = children.shape[0]
-        dsmeta['recordings'] = recordings.dropna(subset = ['recording_filename']).shape[0]
-        dsmeta['duration'] = int(recordings['duration'].sum())
+        dsmeta['total_children'] = children.shape[0]
+        dsmeta['total_recordings'] = recordings.dropna(subset = ['recording_filename']).shape[0]
+        dsmeta['total_duration'] = int(recordings['duration'].sum())
 
         ## Extract languages
         languages = []
         if 'language' in children.columns:
-            languages.extend(list(children['language'].str.strip().unique()))
+            languages.extend(children['language'].str.strip().tolist())
         
         if 'languages' in children.columns:
-            languages.extend(list(set(np.ravel(children['languages'].str.split(';').map(lambda s: s.strip()).values))))
+            languages.extend(np.ravel(children['languages'].str.split(';').map(lambda s: s.strip()).tolist()))
 
-        dsmeta['languages'] = languages
+        dsmeta['languages'] = list(set(languages))
 
         ### Extract devices
         dsmeta['devices'] = list(recordings['recording_device_type'].dropna().unique())
@@ -91,4 +94,65 @@ class MetadataExtractor(BaseMetadataExtractor):
         context.update(vocabulary)
         dsmeta['@context'] = context
 
-        return (dsmeta, [])
+        return dsmeta
+
+    def _get_cnmeta(self, dataset, content):
+        cnmeta = []
+        contents = [{'path': f, 'abspath': os.path.abspath(os.path.join(self.ds.path, f))} for f in self.paths]
+
+        annotations = self.am.annotations
+
+        annotations['abspath'] = annotations.apply(lambda row:
+            os.path.join(
+                self.project.path,
+                'annotations',
+                row['set'],
+                'converted',
+                row['annotation_filename']
+            ),
+            axis = 1
+        )
+        annotations['abspath'] = annotations['abspath'].apply(os.path.abspath)
+        annotations.sort_values('imported_at', inplace = True)
+        annotations.drop_duplicates(
+            'abspath',
+            keep = 'last',
+            inplace = True
+        )
+        annotations = annotations.merge(
+            pd.DataFrame(contents),
+            how = 'inner',
+            left_on = 'abspath',
+            right_on = 'abspath'
+        )
+        annotations['columns'] = annotations['abspath'].apply(lambda f:
+            ','.join(pd.read_csv(f).dropna(axis=1, how='all').columns)
+        )
+        
+        cnmeta.extend([
+            (
+                annotation['path'],
+                {
+                    'set': annotation['set'],
+                    'format': annotation['format'],
+                    'data': annotation['columns'],
+                    'package_version': annotation['package_version'],
+                    'duration': annotation['range_offset']-annotation['range_onset']
+                }
+            )
+            for annotation in annotations.to_dict(orient = 'records')
+        ])
+        
+        return cnmeta
+
+    def get_metadata(self, dataset, content):
+        try:
+            self._load()
+        except Exception as exc:
+            lgr.error("could not read the metadata due to some exception.\n{}".format(str(exc)))
+            return {}, []
+
+        dsmeta = self._get_dsmeta(dataset, content)
+        cnmeta = self._get_cnmeta(dataset, content) if content else []
+
+        return (dsmeta, cnmeta)
